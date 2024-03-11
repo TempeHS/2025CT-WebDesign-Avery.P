93fe0fa51d94880fa06806f3277df979669f1042
AveryPilcer <159206600+AveryPilcer@users.noreply.github.com>
2024-03-11 14:13:58 +1100

    Start layout of game

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let myIntro = 23;
-let enterName = prompt 23;
 
-if (myIntro == enterName)      {
-    alert(myIntro);
+let enterName = prompt ("Enter your name");
+let playerGuess;
+let playerFeedback = "test";
+loadDiv ()
+
+
+
+function loadDiv ()  {
+document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>";
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2> Guess a Number between 1 & 10</H2>";
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback;
+}
+
+function enterNumber ()    {
+    playerGuess = prompt ("Enter a Number")
 }
